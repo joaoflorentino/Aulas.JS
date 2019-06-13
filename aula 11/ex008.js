d5b30045469dc998991a4ef3815568345b39f377
JoaoFlorentino <joao.florentino.senai@outlook.com>
2019-06-13 17:41:51 -0300

    Aula 11 condicoes

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40.9
+console.log(`A velocidade do carro é de ${vel} km/h`)
+if(vel > 60) {
+    console.log('Voce ultralpassou a velocidade maxima.  MULTADO')
+}
+console.log('Dirija usando sempre cinto de segurança')
---
+++ aula 11/ex009.js
@@ -0,0 +1,8 @@
+// Programa usando condição composta
+var pais = 'Brasil'
+console.log(`Vivendo no ${pais}`)
+if(pais == 'Brasil') {
+    console.log('Voce é Brasileiro')
+} else {
+    console.log('Voce é Estrangeiro')
+}
